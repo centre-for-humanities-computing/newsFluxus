fe48ae97b468f02a426cf5772d263cf8f4846dfa
knielbo <kln@cas.au.dk>
2020-06-04 11:58:42 +0200

    add downloader

---
+++ downloader.py
@@ -0,0 +1,15 @@
+import argparse
+import nltk
+import stanfordnlp
+
+def main():
+
+  ap = argparse.ArgumentParser(description="[INFO] language resources to install")
+  ap.add_argument("-l", "--language", required=True, help="language code from StanfordNLP")
+  args = vars(ap.parse_args())
+  
+  nltk.download("stopwords")
+  stanfordnlp.download(args["language"])
+
+if __name__=="__main__":
+    main()
