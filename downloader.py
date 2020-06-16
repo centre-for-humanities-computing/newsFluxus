a5e4146bc8e0eda3fc301f013bdd740302a8e351
knielbo <kln@cas.au.dk>
2020-06-16 11:00:48 +0200

    clean

--- downloader.py
+++ downloader.py
@@ -7,7 +7,7 @@ def main():
   ap = argparse.ArgumentParser(description="[INFO] language resources to install")
   ap.add_argument("-l", "--language", required=True, help="language code from StanfordNLP")
   args = vars(ap.parse_args())
-  
+
   nltk.download("stopwords")
   stanfordnlp.download(args["language"])
 
